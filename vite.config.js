8af90f833cfb3e0d60931ca8c3b55efea70e79ab
DESKTOP-R3K8E88\zhou <87179471@qq.com>
2022-04-03 20:40:00 +0800

    fix dev

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,43 @@
-const path = require('path')
+import { resolve} from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig({
-  plugins: [vue()],
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'src/FirstWidget.vue'),
-      name: 'FirstWidget',
-      formats: ['umd'],
-      fileName: '[name]'
-    },
-    rollupOptions: {
-      external: ['vue', 'naive-ui'],
-      output: {
-        globals: {
-          vue: 'Vue'
+export default defineConfig(({ mode }) => {
+
+  const isProd = mode === 'production'
+  // const isDev = mode === 'development'
+
+  let build = {}
+
+  if (isProd) {
+    build = {
+      lib: {
+        entry: resolve(__dirname, 'src/FirstWidget.vue'),
+        name: 'FirstWidget',
+        formats: ['umd'],
+        fileName: '[name]'
+      },
+      rollupOptions: {
+        external: ['vue', 'naive-ui'],
+        output: {
+          globals: {
+            vue: 'Vue'
+          }
         }
       }
+    }
+  }
+
+  return {
+    plugins: [vue()],
+    build: build,
+    resolve: {
+      alias: [
+        {
+          find: '@',
+          replacement: resolve(__dirname, './src'),
+        },
+      ],
     }
   }
 })
